fa6abe8170dc6b2314a9a50b5e47ea1fcd4f2dc9
Hayaki Saito <user@zuse.jp>
2014-03-29 22:27:48 +0900

    Minor fix for color mapping

--- src/quant.c
+++ src/quant.c
@@ -712,11 +712,7 @@ apply_palette(unsigned char *data,
               int width, int height, int depth,
               unsigned char *palette, int ncolor)
 {
-    int i;
-    int j;
-    int x, y;
-    int r = 0, g = 0, b = 0;
-    int rdiff, gdiff, bdiff;
+    int i, j, n, x, y;
     int roffset, goffset, boffset;
     int distant;
     int diff;
@@ -728,24 +724,21 @@ apply_palette(unsigned char *data,
     for (y = 0; y < height; ++y) {
         for (x = 0; x < width; ++x) {
             i = y * width + x;
-            r = data[i * depth + 0];
-            g = data[i * depth + 1];
-            b = data[i * depth + 2];
-            diff = 256 * 256 * 3;
+            diff = INT_MAX;
             index = -1;
-            j = 0;
-            do {
-                rdiff = r - (int)palette[j * 3 + 0];
-                gdiff = g - (int)palette[j * 3 + 1];
-                bdiff = b - (int)palette[j * 3 + 2];
-                distant = rdiff * rdiff + gdiff * gdiff + bdiff * bdiff;
+            for (j = 0; j < ncolor; ++j) {
+                distant = 0;
+                for (n = 0; n < depth; ++n) {
+                    distant |= (data[i * depth + n] - palette[j * 3 + n])
+                             * (data[i * depth + n] - palette[j * 3 + n]);
+                }
                 if (distant < diff) {
                     diff = distant;
                     index = j;
                 }
-                j++;
-            } while (j != ncolor + 1);
-            if (index != -1) {
+            }
+
+            if (index != -1 && depth == 3) {
                 result[i] = index;
                 if (ncolor > 2) {
                     roffset = (int)data[i * depth + 0] - (int)palette[index * 3 + 0];
@@ -753,23 +746,17 @@ apply_palette(unsigned char *data,
                     boffset = (int)data[i * depth + 2] - (int)palette[index * 3 + 2];
                     if (y < height - 1) {
                         add_offset(data, i + width, depth,
-                                   roffset * 5 / 16,
-                                   goffset * 5 / 16,
-                                   boffset * 5 / 16);
+                                   roffset * 5 / 16, goffset * 5 / 16, boffset * 5 / 16);
                         if (x > 1) {
                             add_offset(data, i + width - 1, depth,
-                                       roffset * 3 / 16,
-                                       goffset * 3 / 16,
-                                       boffset * 3 / 16);
+                                       roffset * 3 / 16, goffset * 3 / 16, boffset * 3 / 16);
                             roffset -= roffset * 3 / 16;
                             goffset -= goffset * 3 / 16;
                             boffset -= boffset * 3 / 16;
                         }
                         if (x < width - 1) {
                             add_offset(data, i + width + 1, depth,
-                                       roffset * 1 / 16,
-                                       goffset * 1 / 16,
-                                       boffset * 1 / 16);
+                                       roffset * 1 / 16, goffset * 1 / 16, boffset * 1 / 16);
                         }
                     }
                     if (x < width - 1) {
@@ -782,10 +769,7 @@ apply_palette(unsigned char *data,
                         roffset -= roffset * 1 / 16;
                         goffset -= goffset * 1 / 16;
                         boffset -= boffset * 1 / 16;
-                        add_offset(data, i + 1, depth,
-                                   roffset,
-                                   goffset,
-                                   boffset);
+                        add_offset(data, i + 1, depth, roffset, goffset, boffset);
                     }
                 }
             }
