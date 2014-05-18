e06ca920a1ab423d618fda088fdbd50437dabd46
Hayaki Saito <user@zuse.jp>
2014-05-18 13:16:01 +0900

    * img2sixel.c: Use gdk-pixbuf and libcurl. * build-gdkpixbuf.sh: Added. this commit is imported from arakiken/libsixel. https://bitbucket.org/arakiken/libsixel/commits/fda0bcab1ec2598bada3d102d8f0cf35d848f6ff

--- converters/img2sixel.c
+++ converters/img2sixel.c
@@ -155,6 +155,17 @@ prepare_specified_palette(char const *mapfile, int reqcolors, int *pncolors)
     return palette;
 }
 
+#ifdef USE_GDK_PIXBUF
+#include <gdk-pixbuf/gdk-pixbuf.h>
+#include <curl/curl.h>
+static size_t
+loader_write(void *data, size_t size, size_t len, void *loader)
+{
+    gdk_pixbuf_loader_write(loader, data, len, NULL) ;
+    return len;
+}
+#endif
+
 
 static int
 convert_to_sixel(char const *filename, int reqcolors,
@@ -188,6 +199,81 @@ convert_to_sixel(char const *filename, int reqcolors,
     } else if (reqcolors > PALETTE_MAX) {
         reqcolors = PALETTE_MAX;
     }
+#ifdef  USE_GDK_PIXBUF
+    {
+        GdkPixbuf *pixbuf;
+        if (strstr(filename, "://")) {
+            CURL *curl;
+            GdkPixbufLoader *loader;
+
+            loader = gdk_pixbuf_loader_new();
+
+			if (pixelwidth > 0 && pixelheight > 0) {
+				gdk_pixbuf_loader_set_size(loader, pixelwidth, pixelheight) ;
+			}
+
+            curl = curl_easy_init();
+            curl_easy_setopt(curl, CURLOPT_URL, filename);
+            curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+            if (strncmp(filename, "https://", 8) == 0) {
+                curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+                curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+            }
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, loader_write);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, loader);
+            curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+
+            gdk_pixbuf_loader_close(loader, NULL);
+
+            if ((pixbuf = gdk_pixbuf_loader_get_pixbuf(loader))) {
+                g_object_ref(pixbuf);
+            }
+
+            g_object_unref(loader);
+        }
+        else
+        {
+            if (pixelwidth > 0 && pixelheight > 0) {
+                pixbuf = gdk_pixbuf_new_from_file_at_scale(filename, pixelwidth, pixelheight, FALSE, NULL);
+            }
+            else {
+                pixbuf = gdk_pixbuf_new_from_file(filename, NULL);
+            }
+        }
+
+        if (pixbuf == NULL) {
+            pixels = NULL;
+        }
+        else {
+            int y;
+            uint8_t *src;
+            uint8_t *dst;
+            sx = gdk_pixbuf_get_width(pixbuf);
+            sy = gdk_pixbuf_get_height(pixbuf);
+            src = dst = pixels = gdk_pixbuf_get_pixels(pixbuf);
+
+            if (gdk_pixbuf_get_has_alpha(pixbuf)) {
+                for (y = 0; y < sy; y++) {
+                    int x;
+                    for (x = 0; x < sx; x++) {
+                        *(dst++) = *(src++);	/* R */
+                        *(dst++) = *(src++);	/* G */
+                        *(dst++) = *(src++);	/* B */
+                        src++;	/* A */
+                    }
+                }
+            }
+            else {
+                size_t rowstride = gdk_pixbuf_get_rowstride(pixbuf);
+                size_t new_rowstride = sx * 3;
+                for (y = 1; y < sy; y++) {
+                    memmove(dst += new_rowstride, src += rowstride, new_rowstride);
+                }
+            }
+        }
+    }
+#else
     f = open_binary_file(filename);
     if (!f) {
         nret = -1;
@@ -226,6 +312,7 @@ convert_to_sixel(char const *filename, int reqcolors,
         free(pixels);
         pixels = scaled_pixels;
     }
+#endif
 
     /* prepare palette */
     if (monochrome) {
@@ -541,7 +628,7 @@ int main(int argc, char *argv[])
             }
             break;
         case 'i':
-	    finvert = 1;
+            finvert = 1;
             break;
         case '?':
             goto argerr;
