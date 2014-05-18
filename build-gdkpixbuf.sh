36ba3710ed984cbecb6608e332f5cda8051eef15
Hayaki Saito <user@zuse.jp>
2014-05-19 01:18:29 +0900

    Drop build-gdkpixbuf.sh

--- build-gdkpixbuf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-CFLAGS="`pkg-config gdk-pixbuf-2.0 --cflags` `pkg-config libcurl --cflags` -DUSE_GDK_PIXBUF -DUSE_LIBCURL" \
-LIBS="`pkg-config gdk-pixbuf-2.0 --libs` `pkg-config libcurl --libs`" \
-./configure ${@}
-make
