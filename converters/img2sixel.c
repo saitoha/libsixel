ea35117d770cba92d51a2262a4a4b6ddf9c286d8
Hayaki Saito <user@zuse.jp>
2014-05-20 00:44:24 +0900

    Initial implementation of gd integration

--- converters/img2sixel.c
+++ converters/img2sixel.c
@@ -260,6 +260,154 @@ load_with_gdk_and_curl(char const *filename, int *psx, int *psy, int *pcomp, int
 }
 #endif  /* HAVE_GDK_PIXBUF2 */
 
+#ifdef HAVE_GD
+
+#define        FMT_GIF     0
+#define        FMT_PNG     1
+#define        FMT_BMP     2
+#define        FMT_JPG     3
+#define        FMT_TGA     4
+#define        FMT_WBMP    5
+#define        FMT_TIFF    6
+#define        FMT_SIXEL   7
+#define        FMT_PNM     8
+#define        FMT_GD2     9
+
+static int
+detect_file_format(int len, unsigned char *data)
+{
+    if (memcmp("TRUEVISION", data + len - 18, 10) == 0)
+        return FMT_TGA;
+
+    if (memcmp("GIF", data, 3) == 0)
+        return FMT_GIF;
+
+    if (memcmp("\x89\x50\x4E\x47\x0D\x0A\x1A\x0A", data, 8) == 0)
+        return FMT_PNG;
+
+    if (memcmp("BM", data, 2) == 0)
+        return FMT_BMP;
+
+    if (memcmp("\xFF\xD8", data, 2) == 0)
+        return FMT_JPG;
+
+    if (memcmp("\x00\x00", data, 2) == 0)
+        return FMT_WBMP;
+
+    if (memcmp("\x4D\x4D", data, 2) == 0)
+        return FMT_TIFF;
+
+    if (memcmp("\x49\x49", data, 2) == 0)
+        return FMT_TIFF;
+
+    if (memcmp("\033P", data, 2) == 0)
+        return FMT_SIXEL;
+
+    if (data[0] == 0x90  && (data[len-1] == 0x9C || data[len-2] == 0x9C))
+        return FMT_SIXEL;
+
+    if (data[0] == 'P' && data[1] >= '1' && data[1] <= '6')
+        return FMT_PNM;
+
+    if (memcmp("gd2", data, 3) == 0)
+        return FMT_GD2;
+
+    return (-1);
+}
+
+
+static unsigned char *
+load_with_gd(char const *filename, int *psx, int *psy, int *pcomp, int *pstride)
+{
+    unsigned char *pixels;
+    int n, len, max;
+    unsigned char *data;
+    gdImagePtr im = NULL;
+
+    f = open_binary_file(filename);
+    if (!f) {
+        return NULL;
+    }
+
+    len = 0;
+    max = 64 * 1024;
+
+    if ((data = (unsigned char *)malloc(max)) == NULL) {
+        return NULL;
+    }
+
+    for (;;) {
+        if ((max - len) < 4096) {
+            max *= 2;
+            if ((data = (BYTE *)realloc(data, max)) == NULL) {
+                return NULL;
+            }
+        }
+        if ((n = fread(data + len, 1, 4096, f)) <= 0) {
+            break;
+        }
+        len += n;
+    }
+
+    if (f != stdout) {
+        fclose(f);
+    }
+
+    switch(detect_file_format(len, data)) {
+        case FMT_GIF:
+            im = gdImageCreateFromGifPtr(len, data);
+            break;
+        case FMT_PNG:
+            im = gdImageCreateFromPngPtr(len, data);
+            break;
+        case FMT_BMP:
+            im = gdImageCreateFromBmpPtr(len, data);
+            break;
+        case FMT_JPG:
+            im = gdImageCreateFromJpegPtrEx(len, data, 1);
+            break;
+        case FMT_TGA:
+            im = gdImageCreateFromTgaPtr(len, data);
+            break;
+        case FMT_WBMP:
+            im = gdImageCreateFromWBMPPtr(len, data);
+            break;
+        case FMT_TIFF:
+            im = gdImageCreateFromTiffPtr(len, data);
+            break;
+        case FMT_SIXEL:
+            im = gdImageCreateFromSixelPtr(len, data);
+            break;
+        case FMT_PNM:
+            im = gdImageCreateFromPnmPtr(len, data);
+            break;
+        case FMT_GD2:
+            im = gdImageCreateFromGd2Ptr(len, data);
+            break;
+    }
+
+    free(data);
+
+    if (im == NULL) {
+        return NULL;
+    }
+
+    if (!gdImageTrueColor(im)) {
+        if (!gdImagePaletteToTrueColor(im)) {
+            return NULL;
+        }
+    }
+
+    *psx = gdImageSX(im);
+    *psy = gdImageSY(im);
+    *pcomp = 3;
+    *pstride = *psx * *pcomp;
+    pixels = (void *)im->pixels;
+    return pixels;
+}
+
+#endif  /* HAVE_GD */
+
 static unsigned char *
 load_image_file(char const *filename, int *psx, int *psy)
 {
@@ -274,6 +422,8 @@ load_image_file(char const *filename, int *psx, int *psy)
 
 #ifdef HAVE_GDK_PIXBUF2
     pixels = load_with_gdk_and_curl(filename, psx, psy, &comp, &stride);
+#elif HAVE_GD
+    pixels = load_with_gd(filename, psx, psy, &comp, &stride);
 #else
     pixels = load_with_stbi(filename, psx, psy, &comp, &stride);
 #endif  /* HAVE_GDK_PIXBUF2 */
