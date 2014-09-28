saitoha/libsixel

commit fb1cd8a852da0ff5ae97684ab7498b9adee09764

Apply arakiken's patch for GNU Screen integration: http://mlterm.sourceforge.net/libsixel-penetrate.patch

src/tosixel.c
@@ -30,10 +30,27 @@
 /* implementation */
 
 static void
+penetrate(sixel_output_t *context, int nwrite)
+{
+    int pos;
+    for (pos = 0; pos < nwrite; pos += 508) {
+        context->fn_write("\x1bP", 2, context->priv);
+        context->fn_write(((char *)context->buffer)+pos,
+                nwrite - pos < 508 ? nwrite - pos : 508, context->priv);
+        context->fn_write("\x1b\\", 2, context->priv);
+    }
+}
+
+
+static void
 sixel_advance(sixel_output_t *context, int nwrite)
 {
     if ((context->pos += nwrite) >= SIXEL_OUTPUT_PACKET_SIZE) {
-        context->fn_write((char *)context->buffer, SIXEL_OUTPUT_PACKET_SIZE, context->priv);
+        if (context->penetrate_multiplexer) {
+            penetrate(context, SIXEL_OUTPUT_PACKET_SIZE);
+        } else {
+            context->fn_write((char *)context->buffer, SIXEL_OUTPUT_PACKET_SIZE, context->priv);
+        }
         memcpy(context->buffer,
                context->buffer + SIXEL_OUTPUT_PACKET_SIZE,
                (context->pos -= SIXEL_OUTPUT_PACKET_SIZE));
@@ -195,7 +212,7 @@ sixel_encode_impl(unsigned char *pixels, int width, int height,
         context->conv_palette[n] = list[n] = n;
     }
 
-    if (context->has_8bit_control) {
+    if (! context->penetrate_multiplexer && context->has_8bit_control) {
         nwrite = sprintf((char *)context->buffer, "\x90" "0;0;0" "q");
     } else {
         nwrite = sprintf((char *)context->buffer, "\x1bP" "0;0;0" "q");
@@ -332,7 +349,16 @@ sixel_encode_impl(unsigned char *pixels, int width, int height,
         memset(map, 0, len);
     }
 
-    if (context->has_8bit_control) {
+    if (context->penetrate_multiplexer) {
+        context->buffer[context->pos] = '\x1b';
+        context->buffer[context->pos + 1] = '\x1b';
+        context->buffer[context->pos + 2] = '\\';
+        context->buffer[context->pos + 3] = '\x1b';
+        context->buffer[context->pos + 4] = 'P';
+        context->buffer[context->pos + 5] = '\\';
+        advance(context, 6);
+    }
+    else if (context->has_8bit_control) {
         context->buffer[context->pos] = '\x9c';
         sixel_advance(context, 1);
     } else {
@@ -346,7 +372,12 @@ sixel_encode_impl(unsigned char *pixels, int width, int height,
 
     /* flush buffer */
     if (context->pos > 0) {
-        context->fn_write((char *)context->buffer, context->pos, context->priv);
+        if (context->penetrate_multiplexer) {
+            penetrate(context, context->pos);
+        }
+        else {
+            context->fn_write((char *)context->buffer, context->pos, context->priv);
+        }
     }
 
     /* free nodes */

converters/img2sixel.c
@@ -190,6 +190,7 @@ typedef struct Settings {
     int clipheight;
     int clipfirst;
     int macro_number;
+    int penetrate_multiplexer;
     int show_version;
     int show_help;
 } settings_t;
@@ -500,6 +501,7 @@ convert_to_sixel(char const *filename, settings_t *psettings)
     if ((psettings->fuse_macro && frame_count > 1) || psettings->macro_number >= 0) {
         context = sixel_output_create(sixel_hex_write_callback, stdout);
         sixel_output_set_8bit_availability(context, psettings->f8bit);
+        sixel_output_set_penetrate_multiplexer(context, psettings->penetrate_multiplexer);
         for (n = 0; n < frame_count; ++n) {
 #if HAVE_USLEEP && HAVE_CLOCK
             start = clock();
@@ -593,6 +595,7 @@ convert_to_sixel(char const *filename, settings_t *psettings)
         /* create output context */
         context = sixel_output_create(sixel_write_callback, stdout);
         sixel_output_set_8bit_availability(context, psettings->f8bit);
+        sixel_output_set_penetrate_multiplexer(context, psettings->penetrate_multiplexer);
         for (c = 0; c != loop_count; ++c) {
             for (n = 0; n < frame_count; ++n) {
                 if (frame_count > 1) {
@@ -846,7 +849,7 @@ main(int argc, char *argv[])
     int number;
     char unit[32];
     int parsed;
-    char const *optstring = "78p:m:ed:f:s:c:w:h:r:q:il:ugn:VH";
+    char const *optstring = "78p:m:ed:f:s:c:w:h:r:q:il:ugn:PVH";
 
     settings_t settings = {
         -1,           /* reqcolors */
@@ -872,6 +875,7 @@ main(int argc, char *argv[])
         0,            /* clipheight */
         0,            /* clipfirst */
         -1,           /* macro_number */
+        0,            /* penetrate_multiplexer */
         0,            /* show_version */
         0,            /* show_help */
     };
@@ -896,6 +900,7 @@ main(int argc, char *argv[])
         {"use-macro",    no_argument,        &long_opt, 'u'},
         {"ignore-delay", no_argument,        &long_opt, 'g'},
         {"macro-number", required_argument,  &long_opt, 'n'},
+        {"penetrate",    no_argument,        &long_opt, 'P'},
         {"version",      no_argument,        &long_opt, 'V'},
         {"help",         no_argument,        &long_opt, 'H'},
         {0, 0, 0, 0}
@@ -1127,6 +1132,9 @@ main(int argc, char *argv[])
             break;
         case 'g':
             settings.fignore_delay = 1;
+            break;
+        case 'P':
+            settings.penetrate_multiplexer = 1;
             break;
         case 'V':
             settings.show_version = 1;

src/output.c
@@ -43,6 +43,7 @@ sixel_output_create(sixel_write_function fn_write, void *priv)
     output->node_free = NULL;
     output->priv = priv;
     output->pos = 0;
+    output->penetrate_multiplexer = 0;
 
     return output;
 }
@@ -82,6 +83,12 @@ void
 sixel_output_set_8bit_availability(sixel_output_t *output, int availability)
 {
     output->has_8bit_control = availability;
+}
+
+void
+sixel_output_set_penetrate_multiplexer(sixel_output_t *output, int penetrate)
+{
+    output->penetrate_multiplexer = penetrate;
 }
 
 

src/output.h
@@ -64,6 +64,8 @@ typedef struct sixel_output {
     sixel_node_t *node_top;
     sixel_node_t *node_free;
 
+    int penetrate_multiplexer;
+
     void *priv;
     int pos;
     unsigned char buffer[1];

commit 3340ebb7ba3330fbe0574a51b77ab7ceba9b28c6

Fix build broken by fb1cd8a

include/sixel.h.in
@@ -126,6 +126,9 @@ sixel_output_get_8bit_availability(sixel_output_t *output);
 void
 sixel_output_set_8bit_availability(sixel_output_t *output, int availability);
 
+void
+sixel_output_set_penetrate_multiplexer(sixel_output_t *output, int penetrate);
+
 #ifdef __cplusplus
 }
 #endif

src/tosixel.c
@@ -356,7 +356,7 @@ sixel_encode_impl(unsigned char *pixels, int width, int height,
         context->buffer[context->pos + 3] = '\x1b';
         context->buffer[context->pos + 4] = 'P';
         context->buffer[context->pos + 5] = '\\';
-        advance(context, 6);
+        sixel_advance(context, 6);
     }
     else if (context->has_8bit_control) {
         context->buffer[context->pos] = '\x9c';