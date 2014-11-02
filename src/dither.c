45245b95d22acdd37383763bf03c6712c3f0d954
Hayaki Saito <user@zuse.jp>
2014-11-03 00:52:10 +0900

    Fix a typo of API signature(#10). old one is still remained.

--- src/dither.c
+++ src/dither.c
@@ -274,10 +274,19 @@ sixel_dither_get_num_of_palette_colors(sixel_dither_t *dither)
 }
 
 
+/* get number of histogram colors */
 int
-sixel_dither_get_num_of_histgram_colors(sixel_dither_t *dither)
+sixel_dither_get_num_of_histogram_colors(sixel_dither_t /* in */ *dither)  /* dither context object */
 {
     return dither->origcolors;
+}
+
+
+/* typoed: remained for keeping compatibility */
+int
+sixel_dither_get_num_of_histgram_colors(sixel_dither_t /* in */ *dither)  /* dither context object */
+{
+    return sixel_dither_get_num_of_histogram_colors(dither);
 }
 
 
