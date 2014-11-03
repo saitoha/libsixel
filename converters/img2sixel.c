207e201c31189b1ae9f38f99887c48dc74e292c0
Hayaki Saito <user@zuse.jp>
2014-11-04 00:29:46 +0900

    Include sys/types.h to use fd_set

--- converters/img2sixel.c
+++ converters/img2sixel.c
@@ -33,6 +33,9 @@
 #if defined(HAVE_SYS_UNISTD_H)
 # include <sys/unistd.h>
 #endif
+#if defined(HAVE_SYS_TYPES_H)
+#include <sys/types.h>
+#endif
 #if defined(HAVE_SYS_SELECT_H)
 #include <sys/select.h>
 #endif
