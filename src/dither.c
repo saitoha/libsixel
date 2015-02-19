cdb06665abe3335146d9d5a7e8cce3b114f11e3c
Hayaki Saito <user@zuse.jp>
2015-02-20 02:51:01 +0900

    Expand 1/2/4 bpp palette with considering surplus bits

--- src/dither.c
+++ src/dither.c
@@ -402,6 +402,9 @@ expand_palette(unsigned char *dst, unsigned char const *src,
 
     switch (pixelformat) {
     case PIXELFORMAT_PAL1:
+#if HAVE_DEBUG
+        printf("Expanding PAL1 to PAL8...\n");
+#endif
         for (y = 0; y < height; ++y) {
             for (x = 0; x < width / 8; ++x) {
                 for (i = 0; i < 8; ++i) {
@@ -410,24 +413,52 @@ expand_palette(unsigned char *dst, unsigned char const *src,
                 src++;
             }
             x = width - x * 8;
-            while (x) {
-                *dst++ = *src >> x-- & 0x1;
+            if (x) {
+                while (x) {
+                    *dst++ = *src >> x-- & 0x1;
+                }
+                src++;
             }
-            src++;
         }
         break;
     case PIXELFORMAT_PAL2:
-        for (i = 0; i < width * height / 4; ++i, ++src) {
-            *dst++ = *src >> 6;
-            *dst++ = *src >> 4 & 0x3;
-            *dst++ = *src >> 2 & 0x3;
-            *dst++ = *src & 0x3;
+#if HAVE_DEBUG
+        printf("Expanding PAL2 to PAL8...\n");
+#endif
+        for (y = 0; y < height; ++y) {
+            for (x = 0; x < width / 4; ++x) {
+                for (i = 0; i < 4; ++i) {
+                    *dst++ = *src >> (3 - i) * 2 & 0x3;
+                }
+                src++;
+            }
+            x = width - x * 4;
+            if (x) {
+                while (x) {
+                    *dst++ = *src >> x-- * 2 & 0x3;
+                }
+                src++;
+            }
         }
         break;
     case PIXELFORMAT_PAL4:
-        for (i = 0; i < width * height / 2; ++i, ++src) {
-            *dst++ = *src >> 4;
-            *dst++ = *src & 0xf;
+#if HAVE_DEBUG
+        printf("Expanding PAL4 to PAL8...\n");
+#endif
+        for (y = 0; y < height; ++y) {
+            for (x = 0; x < width / 2; ++x) {
+                for (i = 0; i < 2; ++i) {
+                    *dst++ = *src >> (1 - i) * 4 & 0xf;
+                }
+                src++;
+            }
+            x = width - x * 2;
+            if (x) {
+                while (x) {
+                    *dst++ = *src >> x-- * 4 & 0xf;
+                }
+                src++;
+            }
         }
         break;
     case PIXELFORMAT_PAL8:
