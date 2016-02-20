7c03450f69609f1b6575e0e3a92676e6863f5e9c
Hayaki Saito <user@zuse.jp>
2016-02-20 16:03:12 +0900

    Update copyright notice

--- src/fromgif.c
+++ src/fromgif.c
@@ -5,7 +5,7 @@
  * Hayaki Saito <saitoha@me.com> modified this and re-licensed
  * it under the MIT license.
  *
- * Copyright (c) 2015 Hayaki Saito
+ * Copyright (c) 2014-2016 Hayaki Saito
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
