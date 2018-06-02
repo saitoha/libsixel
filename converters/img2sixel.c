2b1b7264e829e4705934c0fa2a6700a253255049
Hayaki Saito <saitoha@me.com>
2018-06-02 19:24:38 +0900

    Minor fixes

--- converters/img2sixel.c
+++ converters/img2sixel.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2017 Hayaki Saito
+ * Copyright (c) 2014-2018 Hayaki Saito
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -89,7 +89,7 @@ void show_version(void)
            "no\n"
 #endif
            "\n"
-           "Copyright (C) 2014-2017 Hayaki Saito <saitoha@me.com>.\n"
+           "Copyright (C) 2014-2018 Hayaki Saito <saitoha@me.com>.\n"
            "\n"
            "Permission is hereby granted, free of charge, to any person obtaining a copy of\n"
            "this software and associated documentation files (the \"Software\"), to deal in\n"
