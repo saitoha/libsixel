7f74505c8afc73aafaa92e1206dd30c1540100b6
Hayaki Saito <saitoha@me.com>
2025-09-11 00:00:52 +0900

    gif: use gdk-pixbuf2 or the built-in decoder for gif input when wic/coregraphics/gd are enabled

--- src/loader.c
+++ src/loader.c
@@ -1695,6 +1695,7 @@ sixel_helper_load_image_file(
 {
     SIXELSTATUS status = SIXEL_FALSE;
     sixel_chunk_t *pchunk = NULL;
+    int is_gif;
 
     /* normalize reqested colors */
     if (reqcolors > SIXEL_PALETTE_MAX) {
@@ -1719,9 +1720,10 @@ sixel_helper_load_image_file(
         goto end;
     }
 
+    is_gif = chunk_is_gif(pchunk);
     status = SIXEL_FALSE;
 #ifdef HAVE_WIC
-    if (SIXEL_FAILED(status)) {
+    if (SIXEL_FAILED(status) && !is_gif) {
         status = load_with_wic(pchunk,
                                fstatic,
                                fuse_palette,
@@ -1733,7 +1735,7 @@ sixel_helper_load_image_file(
     }
 #endif  /* HAVE_WIC */
 #ifdef HAVE_COREGRAPHICS
-    if (SIXEL_FAILED(status)) {
+    if (SIXEL_FAILED(status) && !is_gif) {
         status = load_with_coregraphics(pchunk,
                                         fstatic,
                                         fuse_palette,
@@ -1757,7 +1759,7 @@ sixel_helper_load_image_file(
     }
 #endif  /* HAVE_GDK_PIXBUF2 */
 #if HAVE_GD
-    if (SIXEL_FAILED(status)) {
+    if (SIXEL_FAILED(status) && !is_gif) {
         status = load_with_gd(pchunk,
                               fstatic,
                               fuse_palette,
