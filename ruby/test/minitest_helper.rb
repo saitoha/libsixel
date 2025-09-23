527cb780fdad3dd82b94700d25c237e546a10463
Hayaki Saito <saitoha@me.com>
2025-09-24 02:19:18 +0900

    ruby: refine ruby interface.

--- ruby/test/minitest_helper.rb
+++ ruby/test/minitest_helper.rb
@@ -1,4 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'libsixel'
+require 'libsixel/version'
+begin
+  require 'libsixel'
+  $LIBSIXEL_LOADED = true
+rescue LoadError => e
+  $LIBSIXEL_LOADED = false
+  $LIBSIXEL_LOAD_ERROR = e
+end
 
 require 'minitest/autorun'
