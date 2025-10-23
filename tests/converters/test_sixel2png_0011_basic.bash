24a7ca940853f63339cbe47b511ab55f5b53615b
Hayaki Saito <saitoha@me.com>
2025-10-23 14:38:09 +0900

    test: fix images/ path in meson test session

--- tests/converters/test_sixel2png_0011_basic.bash
+++ tests/converters/test_sixel2png_0011_basic.bash
@@ -7,8 +7,8 @@ source "${SCRIPT_DIR}/common.bash"
 
 echo '[test11] sixel2png'
 
-for name in snake.sixel snake2.sixel snake3.sixel; do
-    require_file "${TMP_DIR}/${name}"
+for name in snake.six map8.six map64.six; do
+    require_file "${IMAGES_DIR}/${name}"
 done
 
 expect_failure() {
@@ -26,9 +26,9 @@ expect_failure() {
     rm -f "${output_file}"
 }
 
-expect_failure -i "${TMP_DIR}/unknown.sixel"
+expect_failure -i "${TMP_DIR}/unknown.six"
 output_file="${TMP_DIR}/capture.$$"
-if run_sixel2png -% < "${TMP_DIR}/snake.sixel" >"${output_file}" 2>/dev/null; then
+if run_sixel2png -% < "${TMP_DIR}/snake.six" >"${output_file}" 2>/dev/null; then
     :
 fi
 if [[ -s ${output_file} ]]; then
@@ -38,7 +38,7 @@ if [[ -s ${output_file} ]]; then
 fi
 rm -f "${output_file}"
 output_file="${TMP_DIR}/capture.$$"
-if run_sixel2png invalid_filename < "${TMP_DIR}/snake.sixel" >"${output_file}" 2>/dev/null; then
+if run_sixel2png invalid_filename < "${IMAGES_DIR}/snake.six" >"${output_file}" 2>/dev/null; then
     :
 fi
 if [[ -s ${output_file} ]]; then
@@ -50,7 +50,7 @@ rm -f "${output_file}"
 
 run_sixel2png -H
 run_sixel2png -V
-run_sixel2png < "${TMP_DIR}/snake.sixel" > "${TMP_DIR}/snake1.png"
-run_sixel2png < "${TMP_DIR}/snake2.sixel" > "${TMP_DIR}/snake2.png"
-run_sixel2png - - < "${TMP_DIR}/snake3.sixel" > "${TMP_DIR}/snake3.png"
-run_sixel2png -i "${TMP_DIR}/snake.sixel" -o "${TMP_DIR}/snake4.png"
+run_sixel2png < "${IMAGES_DIR}/snake.six" > "${TMP_DIR}/snake1.png"
+run_sixel2png < "${IMAGES_DIR}/map8.six" > "${TMP_DIR}/snake2.png"
+run_sixel2png - - < "${IMAGES_DIR}/map64.six" > "${TMP_DIR}/snake3.png"
+run_sixel2png -i "${IMAGES_DIR}/snake.six" -o "${TMP_DIR}/snake4.png"
