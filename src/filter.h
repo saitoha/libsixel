849741e0f4e0a82890d09268547814c5874e3c1f
Hayaki Saito <saitoha@me.com>
2025-12-14 03:01:16 +0900

    dag: implement sample filter component

--- src/filter.h
+++ src/filter.h
@@ -63,6 +63,26 @@ typedef struct sixel_filter_io {
 
 typedef struct sixel_filter sixel_filter_t;
 
+typedef struct sixel_filter_sample_config {
+    /*
+     * Visible region used for palette sampling. When width/height are not
+     * positive, the full frame bounds are used instead.
+     */
+    int clip_x;
+    int clip_y;
+    int clip_width;
+    int clip_height;
+
+    /*
+     * Palette sizing knobs carried from encoder options. The override flag
+     * mirrors $SIXEL_PALETTE_SAMPLE_TARGET support.
+     */
+    int reqcolors;
+    int quality_mode;
+    int palette_sample_override;
+    size_t palette_sample_target;
+} sixel_filter_sample_config_t;
+
 typedef SIXELSTATUS (*sixel_filter_apply_fn)(sixel_filter_t *filter,
                                              sixel_allocator_t *allocator,
                                              sixel_logger_t *logger);
@@ -141,6 +161,17 @@ sixel_filter_run(sixel_filter_t *filter,
                  sixel_logger_t *logger);
 
 void sixel_filter_teardown(sixel_filter_t *filter);
+
+SIXELSTATUS
+sixel_filter_sample_init(sixel_filter_t *filter,
+                         const sixel_filter_sample_config_t *config);
+
+SIXELSTATUS
+sixel_filter_sample_frame(const sixel_filter_sample_config_t *config,
+                          sixel_frame_t *frame,
+                          sixel_allocator_t *allocator,
+                          sixel_frame_t **sample_out,
+                          sixel_logger_t *logger);
 
 #endif /* LIBSIXEL_FILTER_H */
 
