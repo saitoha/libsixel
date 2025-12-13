fd18209d47b0369bd7ee9f71f2d38fa4b3222eea
Hayaki Saito <saitoha@me.com>
2025-12-13 12:42:05 +0900

    amalgamation: fix imcompatible prototype of stbi_write_png_to_mem

--- src/writer.c
+++ src/writer.c
@@ -79,7 +79,7 @@
 
 #if !HAVE_LIBPNG
 unsigned char *
-stbi_write_png_to_mem(unsigned char *pixels, int stride_bytes,
+stbi_write_png_to_mem(const unsigned char *pixels, int stride_bytes,
                       int x, int y, int n, int *out_len);
 
 /*
