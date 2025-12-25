5c488aa5689627c52dc4ec61949b22c1da0544ff
Hayaki Saito <saitoha@me.com>
2025-12-25 19:51:39 +0900

    fix: delete "#undef malloc" for cosmopolitan libc

--- src/malloc_stub.c
+++ src/malloc_stub.c
@@ -40,7 +40,6 @@
 #endif  /* HAVE_MEMORY_H */
 
 #if !HAVE_MALLOC
-#undef malloc
 void *
 rpl_malloc(size_t n)
 {
@@ -52,7 +51,6 @@ rpl_malloc(size_t n)
 #endif /* !HAVE_MALLOC */
 
 #if !HAVE_REALLOC
-#undef realloc
 void *
 rpl_realloc(void *p, size_t n)
 {
