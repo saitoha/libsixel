f8d22102a6a9a3faa7c0b9fef753c3e6549d3159
Hayaki Saito <saitoha@me.com>
2025-12-28 19:37:18 +0900

    cosmocc: add the cosmopolitan build script

---
+++ cosmopolitan/build-cosmocc.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+set -euovx
+
+SCRIPTDIR=$(cd $(dirname "${0}") && pwd)
+if which cygpath; then
+  SCRIPTDIR=$(cygpath -u "${SCRIPTDIR}")
+fi
+BUILDDIR="${SCRIPTDIR}"/build
+mkdir -p "${BUILDDIR}"
+
+if ! which cosmocc; then
+  if test ! -f cosmocc.zip; then
+    curl -O https://cosmo.zip/pub/cosmocc/cosmocc.zip
+  fi
+  unzip -d cosmopolitan -o cosmocc.zip
+  chmod +x cosmopolitan/bin/cosmo*
+  PATH=$PWD/cosmopolitan/bin:$PATH
+  export PATH
+fi
+
+ 
+cd "${BUILDDIR}" && (
+  ../../configure \
+    --enable-simd \
+    --disable-shared \
+    --enable-static \
+    --without-png \
+    --without-jpeg \
+    --without-libcurl \
+    --without-onnxruntime \
+    --disable-wiccodec \
+    --disable-appkit \
+    --without-coregraphics \
+    --disable-quicklook-extension \
+    --disable-quicklook-preview \
+    CC=cosmocc \
+    AR=cosmoar \
+    RANLIB=cosmoranlib \
+    INSTALL=cosmoinstall
+  make -j V=1
+  make check -j
+)
