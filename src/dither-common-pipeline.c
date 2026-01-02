05764916087eeadb88d2264c4a16997a86d3f78d
Hayaki Saito <saitoha@me.com>
2026-01-02 16:52:34 +0900

    simd: optimize avx2 palette lane comparison

--- src/dither-common-pipeline.c
+++ src/dither-common-pipeline.c
@@ -105,7 +105,11 @@ sixel_dither_lookup_palette_float32(float const *pixel,
     __m256 diff_r_ps;
     __m256 diff_g_ps;
     __m256 error_ps;
-    float error_block[8];
+    __m256 best_error_ps;
+    __m256 cmp_ps;
+    __m128 error_lo_ps;
+    __m128 error_hi_ps;
+    int cmp_mask;
 #endif
 #if defined(__wasm_simd128__)
     v128_t sample_l_ps;
@@ -192,38 +196,111 @@ sixel_dither_lookup_palette_float32(float const *pixel,
             error_ps = _mm256_fmadd_ps(diff_r_ps, diff_r_ps, error_ps);
             diff_g_ps = _mm256_sub_ps(sample_g_ps, palette_g_ps);
             error_ps = _mm256_fmadd_ps(diff_g_ps, diff_g_ps, error_ps);
-            _mm256_storeu_ps(error_block, error_ps);
-            if (error_block[0] < best_error) {
-                best_error = (double)error_block[0];
-                best_index = color;
-            }
-            if (error_block[1] < best_error) {
-                best_error = (double)error_block[1];
-                best_index = color + 1;
-            }
-            if (error_block[2] < best_error) {
-                best_error = (double)error_block[2];
-                best_index = color + 2;
-            }
-            if (error_block[3] < best_error) {
-                best_error = (double)error_block[3];
-                best_index = color + 3;
-            }
-            if (error_block[4] < best_error) {
-                best_error = (double)error_block[4];
-                best_index = color + 4;
-            }
-            if (error_block[5] < best_error) {
-                best_error = (double)error_block[5];
-                best_index = color + 5;
-            }
-            if (error_block[6] < best_error) {
-                best_error = (double)error_block[6];
-                best_index = color + 6;
-            }
-            if (error_block[7] < best_error) {
-                best_error = (double)error_block[7];
-                best_index = color + 7;
+            best_error_ps = _mm256_set1_ps((float)best_error);
+            cmp_ps = _mm256_cmp_ps(error_ps, best_error_ps,
+                                   _CMP_LT_OQ);
+            cmp_mask = _mm256_movemask_ps(cmp_ps);
+            if (cmp_mask != 0) {
+                /*
+                 * Update best_index directly from the active lanes to avoid
+                 * spilling the vector to a temporary buffer.
+                 */
+                error_lo_ps = _mm256_castps256_ps128(error_ps);
+                if ((cmp_mask & 0x01) != 0
+                        && _mm_cvtss_f32(error_lo_ps) < best_error) {
+                    best_error = (double)_mm_cvtss_f32(error_lo_ps);
+                    best_index = color;
+                }
+                if ((cmp_mask & 0x02) != 0
+                        && _mm_cvtss_f32(_mm_shuffle_ps(error_lo_ps,
+                                                        error_lo_ps,
+                                                        _MM_SHUFFLE(1,
+                                                                    1,
+                                                                    1,
+                                                                    1)))
+                            < best_error) {
+                    best_error = (double)_mm_cvtss_f32(
+                            _mm_shuffle_ps(error_lo_ps,
+                                           error_lo_ps,
+                                           _MM_SHUFFLE(1, 1, 1, 1)));
+                    best_index = color + 1;
+                }
+                if ((cmp_mask & 0x04) != 0
+                        && _mm_cvtss_f32(_mm_shuffle_ps(error_lo_ps,
+                                                        error_lo_ps,
+                                                        _MM_SHUFFLE(2,
+                                                                    2,
+                                                                    2,
+                                                                    2)))
+                            < best_error) {
+                    best_error = (double)_mm_cvtss_f32(
+                            _mm_shuffle_ps(error_lo_ps,
+                                           error_lo_ps,
+                                           _MM_SHUFFLE(2, 2, 2, 2)));
+                    best_index = color + 2;
+                }
+                if ((cmp_mask & 0x08) != 0
+                        && _mm_cvtss_f32(_mm_shuffle_ps(error_lo_ps,
+                                                        error_lo_ps,
+                                                        _MM_SHUFFLE(3,
+                                                                    3,
+                                                                    3,
+                                                                    3)))
+                            < best_error) {
+                    best_error = (double)_mm_cvtss_f32(
+                            _mm_shuffle_ps(error_lo_ps,
+                                           error_lo_ps,
+                                           _MM_SHUFFLE(3, 3, 3, 3)));
+                    best_index = color + 3;
+                }
+                error_hi_ps = _mm256_extractf128_ps(error_ps, 1);
+                if ((cmp_mask & 0x10) != 0
+                        && _mm_cvtss_f32(error_hi_ps) < best_error) {
+                    best_error = (double)_mm_cvtss_f32(error_hi_ps);
+                    best_index = color + 4;
+                }
+                if ((cmp_mask & 0x20) != 0
+                        && _mm_cvtss_f32(_mm_shuffle_ps(error_hi_ps,
+                                                        error_hi_ps,
+                                                        _MM_SHUFFLE(1,
+                                                                    1,
+                                                                    1,
+                                                                    1)))
+                            < best_error) {
+                    best_error = (double)_mm_cvtss_f32(
+                            _mm_shuffle_ps(error_hi_ps,
+                                           error_hi_ps,
+                                           _MM_SHUFFLE(1, 1, 1, 1)));
+                    best_index = color + 5;
+                }
+                if ((cmp_mask & 0x40) != 0
+                        && _mm_cvtss_f32(_mm_shuffle_ps(error_hi_ps,
+                                                        error_hi_ps,
+                                                        _MM_SHUFFLE(2,
+                                                                    2,
+                                                                    2,
+                                                                    2)))
+                            < best_error) {
+                    best_error = (double)_mm_cvtss_f32(
+                            _mm_shuffle_ps(error_hi_ps,
+                                           error_hi_ps,
+                                           _MM_SHUFFLE(2, 2, 2, 2)));
+                    best_index = color + 6;
+                }
+                if ((cmp_mask & 0x80) != 0
+                        && _mm_cvtss_f32(_mm_shuffle_ps(error_hi_ps,
+                                                        error_hi_ps,
+                                                        _MM_SHUFFLE(3,
+                                                                    3,
+                                                                    3,
+                                                                    3)))
+                            < best_error) {
+                    best_error = (double)_mm_cvtss_f32(
+                            _mm_shuffle_ps(error_hi_ps,
+                                           error_hi_ps,
+                                           _MM_SHUFFLE(3, 3, 3, 3)));
+                    best_index = color + 7;
+                }
             }
         }
     }
