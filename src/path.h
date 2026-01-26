f9bcba444a344e4abb2439927a86082c28beb22f
Hayaki Saito <saitoha@me.com>
2026-01-26 15:30:32 +0900

    fix: add missing src/path.{c,h}

---
+++ src/path.h
@@ -0,0 +1,57 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2025 libsixel developers. See `AUTHORS`.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SIXEL_PATH_H
+#define SIXEL_PATH_H
+
+#include <stddef.h>
+
+/*
+ * Convert a path to a libc-friendly representation for the current build.
+ *
+ * Overview (conversion only happens when the build expects a different path
+ * style):
+ *
+ *   +----------------------+-------------------------+---------------------+
+ *   | Build                | Input                   | Output              |
+ *   +----------------------+-------------------------+---------------------+
+ *   | MSVC / MinGW         | /c/...                  | c:/...              |
+ *   | MSVC / MinGW         | /cygdrive/c/...          | c:/...              |
+ *   | Cygwin               | C:\\... or C:/...        | /cygdrive/c/...     |
+ *   | Cygwin               | /cygdrive/c/...          | /cygdrive/c/...     |
+ *   | Any build            | ./a/b or a/b             | (unchanged)         |
+ *   +----------------------+-------------------------+---------------------+
+ *
+ * The helper never allocates. Call sixel_path_to_libc_buffer_size() first to
+ * determine if conversion is needed. If the returned size is 0, the path is
+ * already compatible and the original pointer can be used. Otherwise, allocate
+ * a buffer of that size and pass it to sixel_path_to_libc().
+ */
+size_t sixel_path_to_libc_buffer_size(char const *path);
+
+char const *sixel_path_to_libc(char const *path,
+                               char *buffer,
+                               size_t buffer_size);
+
+#endif  /* SIXEL_PATH_H */
