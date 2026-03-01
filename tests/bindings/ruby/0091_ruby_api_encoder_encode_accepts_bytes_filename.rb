6a6b2851256b6e78e5b240449851f4b1ec98df4a
Hayaki Saito <saitoha@me.com>
2026-03-01 23:02:49 +0900

    tests/ruby: use ARTIFACT_LOCAL_DIR instead of tmpdir

--- tests/bindings/ruby/0091_ruby_api_encoder_encode_accepts_bytes_filename.rb
+++ tests/bindings/ruby/0091_ruby_api_encoder_encode_accepts_bytes_filename.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'tmpdir'
-
 puts '1..1'
 
 begin
@@ -10,19 +8,17 @@ begin
 
   source = File.expand_path('tests/data/inputs/snake_64.png', ENV.fetch('TOP_SRCDIR', Dir.pwd)).dup
   source.force_encoding(Encoding::ASCII_8BIT)
+  artifact_dir = ENV.fetch('ARTIFACT_LOCAL_DIR')
+  output = File.join(artifact_dir, 'encode_bytes_filename.six')
 
-  Dir.mktmpdir('libsixel-ruby-0091') do |tmpdir|
-    output = File.join(tmpdir, 'encode_bytes_filename.six')
+  encoder = Encoder.new
+  encoder.setopt('o', output)
+  encoder.encode(source)
 
-    encoder = Encoder.new
-    encoder.setopt('o', output)
-    encoder.encode(source)
-
-    if File.size(output).positive?
-      puts 'ok 1 - encoder accepts bytes filename input'
-    else
-      puts 'not ok 1 - encoder output missing for bytes filename input'
-    end
+  if File.size(output).positive?
+    puts 'ok 1 - encoder accepts bytes filename input'
+  else
+    puts 'not ok 1 - encoder output missing for bytes filename input'
   end
 rescue StandardError => e
   puts 'not ok 1 - encoder bytes filename acceptance check failed'
--- tests/bindings/ruby/0096_ruby_api_encoder_encode_bytes_accepts_bytearray_palette.rb
+++ tests/bindings/ruby/0096_ruby_api_encoder_encode_bytes_accepts_bytearray_palette.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'tmpdir'
-
 puts '1..1'
 
 begin
@@ -15,28 +13,26 @@ begin
     0, 0, 255,
     255, 255, 255
   ]
+  artifact_dir = ENV.fetch('ARTIFACT_LOCAL_DIR')
+  output = File.join(artifact_dir, 'encode_bytes_bytearray_palette.six')
 
-  Dir.mktmpdir('libsixel-ruby-0096') do |tmpdir|
-    output = File.join(tmpdir, 'encode_bytes_bytearray_palette.six')
+  encoder = Encoder.new
+  encoder.setopt('o', output)
+  encoder.encode_bytes(
+    pixels,
+    width: 4,
+    height: 2,
+    pixelformat: Libsixel::API::SIXEL_PIXELFORMAT_PAL8,
+    palette: palette
+  )
 
-    encoder = Encoder.new
-    encoder.setopt('o', output)
-    encoder.encode_bytes(
-      pixels,
-      width: 4,
-      height: 2,
-      pixelformat: Libsixel::API::SIXEL_PIXELFORMAT_PAL8,
-      palette: palette
-    )
-
-    payload = File.binread(output)
-    if payload.empty?
-      puts 'not ok 1 - encoder output missing for bytearray-like palette input'
-    elsif payload.start_with?("\eP") && payload.end_with?("\e\\")
-      puts 'ok 1 - encoder encode_bytes accepts bytearray-like palette input'
-    else
-      puts 'not ok 1 - encoder output is not a valid sixel envelope'
-    end
+  payload = File.binread(output)
+  if payload.empty?
+    puts 'not ok 1 - encoder output missing for bytearray-like palette input'
+  elsif payload.start_with?("\eP") && payload.end_with?("\e\\")
+    puts 'ok 1 - encoder encode_bytes accepts bytearray-like palette input'
+  else
+    puts 'not ok 1 - encoder output is not a valid sixel envelope'
   end
 rescue StandardError => e
   puts 'not ok 1 - encoder encode_bytes bytearray-like palette acceptance check failed'
