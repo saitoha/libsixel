1cca2c08b7c4b0f4d417cf843546e5c6df1e0d45
Hayaki Saito <saitoha@me.com>
2026-03-06 19:30:36 +0900

    tests: drop runtime_exec()

--- tests/_lib/sh/common.sh
+++ tests/_lib/sh/common.sh
@@ -1,26 +1,18 @@
 #!/bin/sh
 # Common helpers for converter TAP tests executed with POSIX sh.
 
-runtime_exec() {
-    if [ -n "${SIXEL_RUNTIME-}" ]; then
-        "${SIXEL_RUNTIME-}" "$@"
-    else
-        "$@"
-    fi
-}
-
 run_img2sixel() {
-    runtime_exec "${IMG2SIXEL_PATH}" "$@"
+    ${SIXEL_RUNTIME-} "${IMG2SIXEL_PATH}" "$@"
 }
 
 run_sixel2png() {
-    runtime_exec "${SIXEL2PNG_PATH}" "$@"
+    ${SIXEL_RUNTIME-} "${SIXEL2PNG_PATH}" "$@"
 }
 
 run_lsqa() {
-    runtime_exec "${LSQA_PATH}" "$@"
+    ${SIXEL_RUNTIME-} "${LSQA_PATH}" "$@"
 }
 
 run_test_runner() {
-    runtime_exec "${TEST_RUNNER_PATH}" "$@"
+    ${SIXEL_RUNTIME-} "${TEST_RUNNER_PATH}" "$@"
 }
