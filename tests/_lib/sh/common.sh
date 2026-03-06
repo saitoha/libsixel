165fdcad0f87b083fd2f567ae5dde0d74fcfb8a9
Hayaki Saito <saitoha@me.com>
2026-03-06 18:33:49 +0900

    tests: move runtime shlibpath injection to check-TESTS

--- tests/_lib/sh/common.sh
+++ tests/_lib/sh/common.sh
@@ -2,29 +2,6 @@
 # Common helpers for converter TAP tests executed with POSIX sh.
 
 runtime_exec() {
-    runtime_libdir="${TOP_BUILDDIR}/src/.libs"
-    runtime_var="${RUNTIME_SHLIBPATH_VAR:-LD_LIBRARY_PATH}"
-    runtime_sep="${RUNTIME_SHLIBPATH_SEP:-:}"
-    runtime_current=""
-    runtime_value=""
-    runtime_extra="${SIXEL_TEST_ADDITIOANL_PATH:-}"
-    shlibpath_overrides_runpath="${SIXEL_SHLIBPATH_OVERRIDES_RUNPATH:-yes}"
-
-    if [ "${shlibpath_overrides_runpath}" = "yes" ]; then
-        eval "runtime_current=\${${runtime_var}:-}"
-
-        runtime_value="${runtime_libdir}"
-        if [ -n "${runtime_extra}" ]; then
-            runtime_value="${runtime_value}${runtime_sep}${runtime_extra}"
-        fi
-        if [ -n "${runtime_current}" ]; then
-            runtime_value="${runtime_value}${runtime_sep}${runtime_current}"
-        fi
-
-        eval "${runtime_var}=\${runtime_value}"
-        eval "export ${runtime_var}"
-    fi
-
     if [ -n "${SIXEL_RUNTIME-}" ]; then
         "${SIXEL_RUNTIME-}" "$@"
     else
