6ff5d1554773f9b0d6963b82e0a03ee90f30cfe6
Hayaki Saito <saitoha@me.com>
2026-03-31 12:41:37 +0900

    tests: extend TySh grayscale coverage to mode7 rgb/cmyk16/cmyk32

--- tests/data/inputs/formats/generate_psd_snake16_fixtures.py
+++ tests/data/inputs/formats/generate_psd_snake16_fixtures.py
@@ -5203,6 +5203,39 @@ def generate(out_dir: pathlib.Path):
         ),
     )
     write_file(
+        out_dir
+        / "snake16_mode7_rgb8_missing_composite_multilayer_nonpixel_nopixel_tysh_descriptor_gray.psd",
+        build_psd_layer_only_multilayer_custom(
+            color_mode=7,
+            depth=8,
+            channels_header=3,
+            color_mode_data=b"",
+            layers=[
+                {
+                    "top": 0,
+                    "left": 0,
+                    "bottom": HEIGHT,
+                    "right": WIDTH,
+                    "channel_ids": [],
+                    "planes": [],
+                    "blend_key": b"norm",
+                    "additional_blocks": [
+                        (b"TySh", build_descriptor_soco_payload_gray(50.0))
+                    ],
+                },
+                {
+                    "top": 0,
+                    "left": 0,
+                    "bottom": HEIGHT,
+                    "right": WIDTH,
+                    "channel_ids": [0, 1, 2],
+                    "planes": rgb8_planes,
+                    "blend_key": b"norm",
+                },
+            ],
+        ),
+    )
+    write_file(
         out_dir / "snake16_mode7_rgb8_missing_composite_multilayer_fill_soco.psd",
         build_psd_layer_only_multilayer_custom(
             color_mode=7,
@@ -6863,6 +6896,39 @@ def generate(out_dir: pathlib.Path):
         ),
     )
     write_file(
+        out_dir
+        / "snake16_mode7_cmyk16_missing_composite_multilayer_nonpixel_nopixel_tysh_descriptor_gray.psd",
+        build_psd_layer_only_multilayer_custom(
+            color_mode=7,
+            depth=16,
+            channels_header=4,
+            color_mode_data=b"",
+            layers=[
+                {
+                    "top": 0,
+                    "left": 0,
+                    "bottom": HEIGHT,
+                    "right": WIDTH,
+                    "channel_ids": [],
+                    "planes": [],
+                    "blend_key": b"norm",
+                    "additional_blocks": [
+                        (b"TySh", build_descriptor_soco_payload_gray(50.0))
+                    ],
+                },
+                {
+                    "top": 0,
+                    "left": 0,
+                    "bottom": HEIGHT,
+                    "right": WIDTH,
+                    "channel_ids": [0, 1, 2, 3],
+                    "planes": cmyk16_planes,
+                    "blend_key": b"norm",
+                },
+            ],
+        ),
+    )
+    write_file(
         out_dir / "snake16_mode7_cmyk16_missing_composite_multilayer_unknown_blend.psd",
         build_psd_layer_only_multilayer_custom(
             color_mode=7,
@@ -7242,6 +7308,39 @@ def generate(out_dir: pathlib.Path):
                     "blend_key": b"norm",
                     "additional_blocks": [
                         (b"TySh", build_descriptor_soco_payload(255, 48, 64))
+                    ],
+                },
+                {
+                    "top": 0,
+                    "left": 0,
+                    "bottom": HEIGHT,
+                    "right": WIDTH,
+                    "channel_ids": [0, 1, 2, 3],
+                    "planes": cmyk32_planes,
+                    "blend_key": b"norm",
+                },
+            ],
+        ),
+    )
+    write_file(
+        out_dir
+        / "snake16_mode7_cmyk32_missing_composite_multilayer_nonpixel_nopixel_tysh_descriptor_gray.psd",
+        build_psd_layer_only_multilayer_custom(
+            color_mode=7,
+            depth=32,
+            channels_header=4,
+            color_mode_data=b"",
+            layers=[
+                {
+                    "top": 0,
+                    "left": 0,
+                    "bottom": HEIGHT,
+                    "right": WIDTH,
+                    "channel_ids": [],
+                    "planes": [],
+                    "blend_key": b"norm",
+                    "additional_blocks": [
+                        (b"TySh", build_descriptor_soco_payload_gray(50.0))
                     ],
                 },
                 {
